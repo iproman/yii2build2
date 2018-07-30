060883f78b9afd9fc034d14d15c001bc1302da15
iproman <404marao@gmail.com>
2018-07-31 01:34:33 +0300

    Add role model

---
+++ backend/models/Role.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "role".
+ *
+ * @property int $id
+ * @property string $role_name
+ * @property int $role_value
+ */
+class Role extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'role';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['role_value'], 'integer'],
+            [['role_name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'role_name' => 'Role Name',
+            'role_value' => 'Role Value',
+        ];
+    }
+}
