ad03d29b4f09054a9fe250354296de8eb114e6f5
iproman <itechman@yandex.ru>
2018-08-07 23:32:35 +0300

    Add profile model

---
+++ frontend/models/Profile.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "profile".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $birthdate
+ * @property int $gender_id
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Gender $gender
+ */
+class Profile extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'profile';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'gender_id', 'created_at', 'updated_at'], 'required'],
+            [['user_id', 'gender_id'], 'integer'],
+            [['first_name', 'last_name'], 'string'],
+            [['birthdate', 'created_at', 'updated_at'], 'safe'],
+            [['gender_id'], 'exist', 'skipOnError' => true, 'targetClass' => Gender::className(), 'targetAttribute' => ['gender_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'birthdate' => 'Birthdate',
+            'gender_id' => 'Gender ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getGender()
+    {
+        return $this->hasOne(Gender::className(), ['id' => 'gender_id']);
+    }
+}
