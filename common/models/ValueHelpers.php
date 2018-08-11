aca21c7bbe716e89e5d52326e0f6a2993957a1c8
iproman <itechman@yandex.ru>
2018-08-11 22:02:44 +0300

    Add ValueHelper method getStatusValue

--- common/models/ValueHelpers.php
+++ common/models/ValueHelpers.php
@@ -26,13 +26,38 @@ class ValueHelpers
     public static function getRoleValue($role_name)
     {
         $connection = \Yii::$app->db;
-        $sql = "SELECT role_value
-        FROM role
-        WHERE role_name=:role_name";
+        $sql = "
+          SELECT role_value
+          FROM role
+          WHERE role_name=:role_name
+        ";
         $command = $connection->createCommand($sql);
         $command->bindValue(":role_name", $role_name);
         $result = $command->queryOne();
 
         return $result['role_value'];
+    }
+
+    /**
+     * return the value of a status name handed in as string
+     * example: 'Active'
+     *
+     * @param $status_name
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public static function getStatusValue($status_name)
+    {
+        $connection = \Yii::$app->db;
+        $sql = "
+            SELECT status_value
+            FROM status
+            WHERE status_name=:status_name
+        ";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":status_name", $status_name);
+        $result = $command->queryOne();
+
+        return $result['status_value'];
     }
 }
