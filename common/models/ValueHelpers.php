487fd10a888f744ca96f6ef832e9ec49442201ba
iproman <itechman@yandex.ru>
2018-08-11 21:57:15 +0300

    Add ValueHelper method getRoleValue

--- common/models/ValueHelpers.php
+++ common/models/ValueHelpers.php
@@ -15,5 +15,24 @@ namespace common\models;
  */
 class ValueHelpers
 {
+    /**
+     * return the value of a role name handed in as string
+     * example: 'Admin'
+     *
+     * @param $role_name
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public static function getRoleValue($role_name)
+    {
+        $connection = \Yii::$app->db;
+        $sql = "SELECT role_value
+        FROM role
+        WHERE role_name=:role_name";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":role_name", $role_name);
+        $result = $command->queryOne();
 
+        return $result['role_value'];
+    }
 }
