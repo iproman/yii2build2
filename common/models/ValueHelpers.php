c11b174e21b3e3d347a0ab392549d893f77ca254
iproman <itechman@yandex.ru>
2018-08-11 21:56:57 +0300

    Add ValueHelper clas

---
+++ common/models/ValueHelpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iproman
+ * Date: 11.08.2018
+ * Time: 21:49
+ */
+
+namespace common\models;
+
+
+/**
+ * Class ValueHelpers
+ * @package common\models
+ */
+class ValueHelpers
+{
+
+}
