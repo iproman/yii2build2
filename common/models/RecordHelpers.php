c59ee24a791b92391c3a41529c515fce5ee687ce
iproman <itechman@yandex.ru>
2018-08-14 01:25:16 +0300

    Add RecordHelpers method useHas

--- common/models/RecordHelpers.php
+++ common/models/RecordHelpers.php
@@ -8,8 +8,28 @@
 
 namespace common\models;
 
+use yii;
 
 class RecordHelpers
 {
+    /**
+     * @param $model_name
+     * @return bool
+     * @throws yii\db\Exception
+     */
+    public static function userHas($model_name)
+    {
+        $connection = \Yii::$app->db;
+        $userid = Yii::$app->user->identity->id;
+        $sql = "SELECT id FROM $model_name WHERE user_id=:userid";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":userid", $userid);
+        $result = $command->queryOne();
 
+        if ($result == null) {
+            return false;
+        } else {
+            return $result['id'];
+        }
+    }
 }
