232c5ee27df7eaa189d74a374a92496b54819185
iproman <itechman@yandex.ru>
2018-08-15 22:43:22 +0300

    Some fix

--- frontend/models/PasswordResetRequestForm.php
+++ frontend/models/PasswordResetRequestForm.php
@@ -39,7 +39,7 @@ class PasswordResetRequestForm extends Model
     {
         /* @var $user User */
         $user = User::findOne([
-            'status' => User::STATUS_ACTIVE,
+            'status_id' => User::STATUS_ACTIVE,
             'email' => $this->email,
         ]);
 
