396c79b8600d0349b56732ffcc86bb0bc6248cd4
iproman <itechman@yandex.ru>
2018-08-23 00:51:53 +0300

    Add login() validate statusValue = Active

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -56,8 +56,11 @@ class LoginForm extends Model
      */
     public function login()
     {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        if ($this->validate() && $this->getUser()->status_id ==
+            ValueHelpers::getStatusValue('Active')) {
+
+            return Yii::$app->user->login($this->getUser(),
+                $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
         
         return false;
