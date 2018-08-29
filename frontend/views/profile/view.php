7c1d3a635863453206dddfb00ed59d21d18188f6
iproman <itechman@yandex.ru>
2018-08-30 02:41:57 +0300

    Internationalisation delete confirmation

--- frontend/views/profile/view.php
+++ frontend/views/profile/view.php
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
                 'method' => 'post',
             ],
         ]) ?>
