51331f9b3d2a32741df102262477eb5ffa708273
iproman <itechman@yandex.ru>
2018-09-03 01:23:55 +0300

    Add behaviours to UpgradeController

--- frontend/controllers/UpgradeController.php
+++ frontend/controllers/UpgradeController.php
@@ -2,10 +2,45 @@
 
 namespace frontend\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\PermissionHelpers;
+use common\models\RecordHelpers;
+use frontend\models\Profile;
 
 class UpgradeController extends Controller
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access2' => [
+                'class' => AccessControl::class,
+                'only' => ['index', 'view', 'create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                        'matchCallback' => function ($rule, $action) {
+                            return PermissionHelpers::requireStatus('Active');
+                        }
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
