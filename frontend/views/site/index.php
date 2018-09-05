0bb5379296d597ec6197370844898661d3b267b2
iproman <itechman@yandex.ru>
2018-09-06 02:50:12 +0300

    Some changes into frontend/view

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,55 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+use yii\bootstrap\Modal;
+use yii\bootstrap\Collapse;
+use yii\bootstrap\Alert;
+use kartik\social\FacebookPlugin;
+
+$this->title = Yii::$app->params['brandLabel'];
 ?>
 <div class="site-index">
-
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <?php If (Yii::$app->user->isGuest) {
+            echo Html::a(
+                'Get Started Today',
+                ['site/signup'],
+                ['class' => 'btn btn-lg btn-success']);
+        }; ?>
+        <h1><?= Yii::$app->params['brandLabel'] ?></h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Use this Yii 2 Template to start Project</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <br>
+        <?= Alert::widget([
+            'options' => [
+                'class' => 'alert-info',
+            ],
+            'body' => 'Launch your project like a rocket ...',
+        ]); ?>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+</div>
+<div class="body-content">
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>Free</h2>
+            <p>
+                <?php
+                if (!Yii::$app->user->isGuest) {
+                    echo Yii::$app->user->identity->username . ' is doing cool stuff. ';
+                }
+                ?>
+                Starting with this free, open source Yii 2 template and it will save you
+                a lot of time. You can deliver projects to the customer quickly, with
+                a lot of boilerplate already taken care of for you, so you can concentrate
+                on the complicated stuff.
+            </p>
+            <p>
+                <a href="http://www.yiiframework.com/doc/" class="btn btn-default">
+                    Yii Documentation &raquo;
+                </a>
+            </p>
         </div>
-
     </div>
 </div>
