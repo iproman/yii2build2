38e08fbec3d2df8eaef1d26a2d9ff6b8173c45e2
iproman <itechman@yandex.ru>
2018-09-10 04:04:50 +0300

    Some change in profile view

--- backend/views/profile/view.php
+++ backend/views/profile/view.php
@@ -2,40 +2,44 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use common\models\PermissionHelpers;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Profile */
 
 $this->title = $model->id;
+$show_this_nav = PermissionHelpers::requireMinimumRole('SuperUser');
 $this->params['breadcrumbs'][] = ['label' => 'Profiles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="profile-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Profile: <?= Html::encode($this->title) ?></h1>
+    <?php if (!Yii::$app->user->isGuest && $show_this_nav) {
+        echo Html::a('Update',
+            ['update', 'id' => $model->id],
+            ['class' => 'btn btn-primary']);
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        echo Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
-        ]) ?>
-    </p>
+        ]);
+    } ?>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
-            'user_id',
-            'first_name:ntext',
-            'last_name:ntext',
+            ['attribute' => 'userLink', 'format' => 'raw'],
+            'first_name',
+            'last_name',
             'birthdate',
-            'gender_id',
+            'gender.gender_name',
             'created_at',
             'updated_at',
+            'id',
         ],
     ]) ?>
 
