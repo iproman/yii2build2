1baa6cbaf69a90509febafb84a75d0b40ec97229
iproman <itechman@yandex.ru>
2018-09-13 02:36:00 +0300

    Update profile _search

--- backend/views/profile/_search.php
+++ backend/views/profile/_search.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use frontend\models\Profile;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\ProfileSearch */
@@ -15,21 +16,14 @@ use yii\widgets\ActiveForm;
         'method' => 'get',
     ]); ?>
 
-    <?= $form->field($model, 'id') ?>
-
-    <?= $form->field($model, 'user_id') ?>
-
     <?= $form->field($model, 'first_name') ?>
 
     <?= $form->field($model, 'last_name') ?>
 
     <?= $form->field($model, 'birthdate') ?>
 
-    <?php // echo $form->field($model, 'gender_id') ?>
-
-    <?php // echo $form->field($model, 'created_at') ?>
-
-    <?php // echo $form->field($model, 'updated_at') ?>
+    <?php echo $form->field($model, 'gender_id')
+        ->dropDownList(Profile::getGenderList(), ['prompt' => 'Please Choose One']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
