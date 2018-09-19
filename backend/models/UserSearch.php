318cba70f0c4331db06694914c0aff28dbee3f42
iproman <itechman@yandex.ru>
2018-09-19 23:41:49 +0300

    Modify User search model

--- backend/models/UserSearch.php
+++ backend/models/UserSearch.php
@@ -5,12 +5,12 @@ namespace backend\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\user;
+use common\models\User;
 
 /**
  * UserSearch represents the model behind the search form of `common\models\user`.
  */
-class UserSearch extends user
+class UserSearch extends User
 {
 
     public $roleName;
@@ -103,26 +103,60 @@ class UserSearch extends user
         ]);
 
         if (!($this->load($params) && !$this->validate())) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
+            $query->joinWith(['role'])
+                ->joinWith(['status'])
+                ->joinWith(['profile'])
+                ->joinWith(['userType']);
+
             return $dataProvider;
         }
 
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'role_id' => $this->role_id,
-            'status_id' => $this->status_id,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ]);
+        $this->addSearchParameter($query, 'user.id');
+        $this->addSearchParameter($query, 'username', true);
+        $this->addSearchParameter($query, 'email', true);
+        $this->addSearchParameter($query, 'role_id');
+        $this->addSearchParameter($query, 'status_id');
+        $this->addSearchParameter($query, 'user_type_id');
+        $this->addSearchParameter($query, 'created_at');
+        $this->addSearchParameter($query, 'updated_at');
 
-        $query->andFilterWhere(['like', 'username', $this->username])
-            ->andFilterWhere(['like', 'auth_key', $this->auth_key])
-            ->andFilterWhere(['like', 'password_hash', $this->password_hash])
-            ->andFilterWhere(['like', 'password_reset_token', $this->password_reset_token])
-            ->andFilterWhere(['like', 'email', $this->email]);
+        // filter by role
+        $query->joinWith(['role' => function ($q) {
+            $q->where('role.role_name LIKE "%' . $this->roleName . '%"');
+        }])
+            // filter by status
+            ->joinWith(['status' => function ($q) {
+                $q->where('status.status_name LIKE "%' . $this->statusName . '%"');
+            }])
+            // filter by user type
+            ->joinWith(['userType' => function ($q) {
+                $q->where('user_type.user_type_name LIKE "%' . $this->userTypeName . '%"');
+            }]);
 
         return $dataProvider;
+    }
+
+    /**
+     * @param $query
+     * @param $attribute
+     * @param bool $partialMatch
+     */
+    protected function addSearchParameter($query, $attribute, $partialMatch = false)
+    {
+        if (($pos = strpos($attribute, '.', )) !== false) {
+            $modelAttribute = substr($attribute, $pos + 1);
+        } else {
+            $modelAttribute = $attribute;
+        }
+        $value = $this->$modelAttribute;
+        if (trim($value) === '') {
+            return;
+        }
+        $attribute = "user.$attribute";
+        if ($partialMatch) {
+            $query->andWhere(['like', $attribute, $value]);
+        } else {
+            $query->andWhere([$attribute => $value]);
+        }
     }
 }
